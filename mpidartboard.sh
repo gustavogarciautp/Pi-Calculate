48f56781f34e5c3c02895360bb844b115dd6bfdb
danielBedoya <danielbedoyatbs@utp.edu.co>
2019-06-24 17:08:59 +0000

    actualizo los CSV con el otro cluster

--- mpidartboard.sh
+++ mpidartboard.sh
@@ -3,6 +3,6 @@ for i in {1..10}
 do
 	for j in {300000, 500000, 1000000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000, 4500000, 5000000}
 	do
-		mpirun -hostfile hosts --prefix /home/andressone9/mpi -np 3 /home/andressone9/hpc2/Pi-Calculate/dartboardmpi $j dartmpi.csv
+		mpirun -hostfile hosts --prefix /home/cluster/mirror/mpi -np 3 dartboardmpi $j dartmpi.csv
 	done
 done
